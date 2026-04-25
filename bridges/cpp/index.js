bc7036d31f9506b2ebea0ed301d31b21e03d8737
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2026-04-25 23:43:45 +0200

    adds silent failure prevention

--- bridges/cpp/index.js
+++ bridges/cpp/index.js
@@ -1,1 +1,8 @@
-export default {}
+export const metrics = () => { throw new Error('CPP bridge not implemented yet') }
+export const load = () => { throw new Error('CPP bridge not implemented yet') }
+export const unload = () => { throw new Error('CPP bridge not implemented yet') }
+export const abort = () => { throw new Error('CPP bridge not implemented yet') }
+export async function generate () { throw new Error('CPP bridge not implemented yet') }
+export async function * stream () { throw new Error('CPP bridge not implemented yet') }
+
+export default { metrics, load, unload, abort, generate, stream }
