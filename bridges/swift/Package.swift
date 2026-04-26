994ab51a22aebd842ff76e8127878cb6769e08ce
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2026-04-26 20:33:38 +0200

    removing obsolete dependencies

--- bridges/swift/Package.swift
+++ bridges/swift/Package.swift
@@ -9,14 +9,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ml-explore/mlx-swift-lm", branch: "main"),
-        // .package(url: "https://github.com/huggingface/swift-transformers", from: "1.3.0"),
     ],
     targets: [
         .target(
             name: "MLXBridge",
             dependencies: [
                 .product(name: "MLXLLM", package: "mlx-swift-lm"),
-                .product(name: "MLXLMCommon", package: "mlx-swift-lm")
+                .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
             ],
             path: "Sources",
         ),
