e8b58adb555c06b4cc1e0965f07c69b2173dab27
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2026-05-09 16:31:38 +0200

    adds models downloader for testing

---
+++ packages/tool/test/models/download.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+INPUT_FILE="readme.md"
+
+if [[ ! -f "$INPUT_FILE" ]]; then
+    echo "Error: $INPUT_FILE not found."
+    exit 1
+fi
+
+grep -o 'https://huggingface.co/[^ /]*/[^ /]*' "$INPUT_FILE" | xargs -n 1 -P 1 bash -c '
+    REPO_URL="$1"
+    DIR_NAME=$(basename "$REPO_URL")
+
+    mkdir -p "$DIR_NAME"
+    echo "📂 Processing $DIR_NAME..."
+
+    FILES=(
+        "tokenizer.json"
+        "tokenizer_config.json"
+        "chat_template.jinja"
+    )
+
+    for FILE in "${FILES[@]}"; do
+        DL_URL="$REPO_URL/resolve/main/$FILE"
+        if curl --output /dev/null --silent --head --fail "$DL_URL"; then
+            echo "  ✅ Downloading $FILE -> $DIR_NAME"
+            curl -L -s "$DL_URL" -o "$DIR_NAME/$FILE"
+        fi
+    done
+' _
---
+++ packages/tool/test/models/readme.md
@@ -0,0 +1,11 @@
+## Download
+To download the assets required to run the tests, run `bash download.sh`
+
+- [ ] https://huggingface.co/mlx-community/Qwen3.5-9B-6bit
+- [ ] https://huggingface.co/mlx-community/granite-4.1-8b-8bit
+- [ ] https://huggingface.co/Jackrong/MLX-Qwen3.5-9B-Claude-4.6-Opus-Reasoning-Distilled-8bit
+- [ ] https://huggingface.co/mistralai/Ministral-8B-Instruct-2410
+- [ ] https://huggingface.co/mlx-community/Meta-Llama-3.1-8B-Instruct-8bit
+- [ ] https://huggingface.co/openai/gpt-oss-20b
+- [ ] https://huggingface.co/Qwen/Qwen3.6-27B
+- [ ] https://huggingface.co/mlx-community/gemma-4-e4b-6bit
