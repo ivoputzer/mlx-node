4c9b9a122355854008fb7644dfed4c57abd31e7b
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2026-05-13 15:13:01 +0200

    updates eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,9 @@
-import neostandard from 'neostandard'
-export default neostandard()
+import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
+
+const ignores = resolveIgnoresFromGitignore()
+export default [
+  ...neostandard({ ignores }),
+  {
+    languageOptions: { ecmaVersion: 2025 }
+  }
+]
