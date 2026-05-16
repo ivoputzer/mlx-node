fbcdad2135c1823a107226ad06d1e7d02bb69d71
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2026-05-16 16:07:52 +0200

    uniforms testing

--- bridges/swift/index.test.js
+++ bridges/swift/index.test.js
@@ -8,26 +8,26 @@ const require = createRequire(import.meta.url)
 const native = require('mlx-swift')
 
 describe('mlx-swift', () => {
-  it('should contain all keys present in the native module', () => {
+  it('contains all keys present in the native module', () => {
     for (const key of Object.getOwnPropertyNames(native)) {
       ok(key in mlx, `Driver is missing key: ${key}`)
       strictEqual(typeof mlx[key], typeof native[key], `Type mismatch for key: ${key}`)
     }
   })
 
-  it('should maintain functional parity with native methods', () => {
+  it('maintains functional parity with native methods', () => {
     const { version } = require('./package.json')
     strictEqual(mlx.version, version, 'Native method call failed through driver')
   })
 
-  describe('Mocking Capabilities', () => {
-    it('should allow mocking methods', ({ mock }) => {
+  describe('mocking', () => {
+    it('allows mocking methods', ({ mock }) => {
       mock.method(mlx, 'systemMetrics', Function.prototype)
       strictEqual(mlx.systemMetrics(), undefined, 'The method was not successfully mocked')
       strictEqual(mlx.systemMetrics.mock.calls.length, 1)
     })
 
-    it('should have configurable property descriptors', () => {
+    it('has configurable property descriptors', () => {
       // N-API default properties are usually non-configurable.
       // Our wrapper should have made them configurable.
       const firstKey = Object.keys(mlx)[0]
